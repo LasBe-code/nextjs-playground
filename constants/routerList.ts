44347847b1b554a2e637d104a6ea54e200963be8
lasbe <devlasbe@gmail.com>
2024-09-09 04:57:41 +0900

    rename: 페이지명 띄어쓰기 스네이크 케이스로 변경

--- constants/routerList.ts
+++ constants/routerList.ts
@@ -6,22 +6,22 @@ const routerList = [
   },
   {
     label: "Api Method",
-    path: "/1-api-method",
+    path: "/1-api_method",
     desc: "내장 API 사용 및 method",
   },
   {
     label: "Server Rendering",
-    path: "/2-server-rendering",
+    path: "/2-server_rendering",
     desc: "서버 렌더링 및 prefetch",
   },
   {
     label: "Dynamic Routing",
-    path: "/3-dynamic-routes",
+    path: "/3-dynamic_routes",
     desc: "동적 라우팅",
   },
   {
     label: "SearchParam",
-    path: "/4-search-param",
+    path: "/4-search_param",
     desc: "쿼리 스트링 파싱",
   },
 ];
